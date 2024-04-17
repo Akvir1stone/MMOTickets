7998c4368587af550707f02616c92487a7b0e64b
Akvir1stone <minnikaiev@mail.ru>
2024-04-17 20:31:01 +0300

    1

--- MMOTickets/MMOTickets/urls.py
+++ MMOTickets/MMOTickets/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
+    path('', include('tickets.urls')),
 ]
+
+urlpatterns += [
+    path("ckeditor5/", include('django_ckeditor_5.urls'), name="ck_editor_5_upload_file"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- MMOTickets/tickets/models.py
+++ MMOTickets/tickets/models.py
@@ -1,19 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django_ckeditor_5.fields import CKEditor5Field
+
 # Create your models here.
 
 
 class Category(models.Model):
     category_name = models.CharField(max_length=40)
 
+    def __str__(self):  # TODO Проверить действительно ли этот метот нужен
+        return self.category_name
+
 
 class Ticket(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     head = models.CharField(max_length=200)
-    body = 0  # TODO add django-ckeditor field
+    body = CKEditor5Field()
     category = models.ManyToManyField(Category, through='TicketCategory')
     responders = models.ManyToManyField(User, through='TicketResponders')
+    status = models.BooleanField(default=True)
+
+    def close_ticket(self):
+        self.status = False
+        self.save()
 
 
 class TicketCategory(models.Model):
